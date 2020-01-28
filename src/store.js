5cca1e9ee73cf95c388cfdc5c9224b0430f45409
Kori Gandolph <korigandolph@Koris-MacBook-Air.local>
2020-01-28 16:15:46 -0700

    done

--- src/store.js
+++ src/store.js
@@ -4,13 +4,19 @@ const initialState={
     name: '',
     category: '',
     authorFirst: '',
-    authorLast: ''
+    authorLast: '',
+    ingredients: [],
+    instructions: [], 
+    recipes: []
 };
 
 export const UPDATE_NAME = 'UPDATE_NAME';
 export const UPDATE_CATEGORY = 'UPDATE_CATEGORY';
 export const UPDATE_AUTHOR_FIRST = 'UPDATE_AUTHOR_FIRST';
 export const UPDATE_AUTHOR_LAST = 'UPDATE_AUTHOR_LAST';
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INSTRUCTION = 'ADD_INSTRUCTION';
+export const ADD_RECIPE = 'ADD_RECIPE';
 
 function reducer(state=initialState, action){
     const {type, payload}= action;
@@ -23,6 +29,30 @@ function reducer(state=initialState, action){
             return {...state, authorFirst: payload};
         case UPDATE_AUTHOR_LAST: 
             return {...state, authorLast: payload};
+        case ADD_INGREDIENT:
+            return {...state, ingredients: newIngredients};
+        case ADD_INSTRUCTION:
+            const newInstructions = [...state.instructions, payload];
+            return {...state, instructions: newInstructions};
+        case ADD_RECIPE:
+            const{
+                name, 
+                category,
+                authorFirst,
+                authorLast,
+                ingredients,
+                instructions
+            } = state;
+            const recipe = {
+                name, 
+                category,
+                authorFirst,
+                authorLast,
+                ingredients,
+                instructions
+            };
+            const newRecipes = [...state.recipes, recipe];
+                return {...state, recipes: newRecipes};
         default:
             return state;
     }
